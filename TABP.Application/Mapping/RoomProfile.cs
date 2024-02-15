5597acd02b15a52ef1ed27c76248a08405b18326
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:50:32 +0200

    Add RoomProfile mapping profile

---
+++ TABP.Application/Mapping/RoomProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using TABP.Application.Rooms.Create;
+using TABP.Application.Rooms.GetByRoomClassIdForGuest;
+using TABP.Application.Rooms.GetForManagement;
+using TABP.Application.Rooms.Update;
+using TABP.Domain.Entities;
+using TABP.Domain.Models;
+
+namespace TABP.Application.Mapping;
+
+public class RoomProfile : Profile
+{
+  public RoomProfile()
+  {
+    CreateMap<PaginatedList<Room>, PaginatedList<RoomForGuestResponse>>()
+      .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+    CreateMap<PaginatedList<RoomForManagement>, PaginatedList<RoomForManagementResponse>>()
+      .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+    CreateMap<CreateRoomCommand, Room>();
+    CreateMap<UpdateRoomCommand, Room>();
+    CreateMap<Room, RoomForGuestResponse>();
+    CreateMap<RoomForManagement, RoomForManagementResponse>();
+  }
+}

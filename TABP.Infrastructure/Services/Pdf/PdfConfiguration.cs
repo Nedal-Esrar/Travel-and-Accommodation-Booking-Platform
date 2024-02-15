05d836cd3a34b8d1445cfa546b17b156d187aea3
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 14:50:51 +0200

    Add PDF infrastructure

---
+++ TABP.Infrastructure/Services/Pdf/PdfConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TABP.Domain.Interfaces.Services;
+
+namespace TABP.Infrastructure.Services.Pdf;
+
+public static class PdfConfiguration
+{
+  public static IServiceCollection AddPdfInfrastructure(this IServiceCollection services)
+  {
+    services.AddScoped<IPdfService, PdfService>();
+
+    return services;
+  }
+}
---
+++ TABP.Infrastructure/Services/Pdf/PdfService.cs
@@ -0,0 +1,17 @@
+﻿using NReco.PdfGenerator;
+using TABP.Domain.Interfaces.Services;
+
+namespace TABP.Infrastructure.Services.Pdf;
+
+public class PdfService : IPdfService
+{
+  public async Task<byte[]> GeneratePdfFromHtmlAsync(string html, CancellationToken cancellationToken = default)
+  {
+    return await Task.Run(() =>
+    {
+      var htmlToPdfConverter = new HtmlToPdfConverter();
+
+      return htmlToPdfConverter.GeneratePdf(html);
+    }, cancellationToken);
+  }
+}

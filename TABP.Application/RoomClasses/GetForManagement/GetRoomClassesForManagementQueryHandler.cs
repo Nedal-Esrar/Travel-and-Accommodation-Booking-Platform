ae711ec91b1a7c5846eb49c98fce8df5b03024ca
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:31:41 +0200

    Add get room classes for manangement query and handler

---
+++ TABP.Application/RoomClasses/GetForManagement/GetRoomClassesForManagementQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using MediatR;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Models;
+
+namespace TABP.Application.RoomClasses.GetForManagement;
+
+public class GetRoomClassesForManagementQueryHandler : IRequestHandler<GetRoomClassesForManagementQuery,
+  PaginatedList<RoomClassForManagementResponse>>
+{
+  private readonly IMapper _mapper;
+  private readonly IRoomClassRepository _roomClassRepository;
+
+  public GetRoomClassesForManagementQueryHandler(IRoomClassRepository roomClassRepository, IMapper mapper)
+  {
+    _roomClassRepository = roomClassRepository;
+    _mapper = mapper;
+  }
+
+  public async Task<PaginatedList<RoomClassForManagementResponse>> Handle(GetRoomClassesForManagementQuery request,
+    CancellationToken cancellationToken)
+  {
+    var query = new PaginationQuery<RoomClass>(
+      GetSearchExpression(request.SearchTerm),
+      request.SortOrder ?? SortOrder.Ascending,
+      request.SortColumn,
+      request.PageNumber,
+      request.PageSize);
+
+    var owners = await _roomClassRepository.GetAsync(
+      query,
+      false,
+      cancellationToken);
+
+    return _mapper.Map<PaginatedList<RoomClassForManagementResponse>>(owners);
+  }
+
+  private static Expression<Func<RoomClass, bool>> GetSearchExpression(string? searchTerm)
+  {
+    return searchTerm is null
+      ? _ => true
+      : rc => rc.Name.Contains(searchTerm);
+  }
+}

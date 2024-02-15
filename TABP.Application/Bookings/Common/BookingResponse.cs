e9913c331a41797864638d13b80a6d6a62261e61
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:57:42 +0200

    Add create booking command and handler with its details

---
+++ TABP.Application/Bookings/Common/BookingResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TABP.Application.Bookings.Common;
+
+public record BookingResponse(
+  Guid Id,
+  string HotelName,
+  decimal TotalPrice,
+  DateOnly CheckInDateUtc,
+  DateOnly CheckOutDateUtc,
+  DateOnly BookingDateUtc);
---
+++ TABP.Application/Bookings/Create/BookingEmail.cs
@@ -0,0 +1,16 @@
+﻿using TABP.Domain.Models;
+
+namespace TABP.Application.Bookings.Create;
+
+public static class BookingEmail
+{
+  public static EmailRequest GetBookingEmailRequest(string toEmail,
+    IEnumerable<(string name, byte[] file)> attachments)
+  {
+    return new EmailRequest(
+      [toEmail],
+      "Reservation is Confirmed!",
+      "Thank you for reserving on our platform, here is the invoice",
+      attachments);
+  }
+}

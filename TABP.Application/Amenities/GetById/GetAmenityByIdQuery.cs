5aaa2ee632a2c9e9181569c3539e730ae4928b8a
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 12:49:25 +0200

    Add get amenity by id query and handler

---
+++ TABP.Application/Amenities/GetById/GetAmenityByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TABP.Application.Amenities.Common;
+
+namespace TABP.Application.Amenities.GetById;
+
+public record GetAmenityByIdQuery(Guid AmenityId) : IRequest<AmenityResponse>;

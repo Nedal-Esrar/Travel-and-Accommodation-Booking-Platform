686ff596d3971477c4d702a620474cc5b9e967ff
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:27:55 +0200

    Add update roomClass command and handler

---
+++ TABP.Application/RoomClasses/Update/UpdateRoomClassHandler.cs
@@ -0,0 +1,48 @@
+using AutoMapper;
+using MediatR;
+using TABP.Domain.Exceptions;
+using TABP.Domain.Interfaces.Persistence;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Messages;
+
+namespace TABP.Application.RoomClasses.Update;
+
+public class UpdateRoomClassHandler : IRequestHandler<UpdateRoomClassCommand>
+{
+  private readonly IMapper _mapper;
+  private readonly IRoomClassRepository _roomClassRepository;
+  private readonly IUnitOfWork _unitOfWork;
+
+  public UpdateRoomClassHandler(
+    IRoomClassRepository roomClassRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper)
+  {
+    _roomClassRepository = roomClassRepository;
+    _unitOfWork = unitOfWork;
+    _mapper = mapper;
+  }
+
+  public async Task Handle(
+    UpdateRoomClassCommand request,
+    CancellationToken cancellationToken)
+  {
+    var roomClassEntity = await _roomClassRepository.GetByIdAsync(request.RoomClassId, cancellationToken);
+
+    if (roomClassEntity is null)
+    {
+      throw new NotFoundException(RoomClassMessages.NotFound);
+    }
+
+    if (await _roomClassRepository.ExistsByNameInHotelAsync(roomClassEntity.HotelId, request.Name, cancellationToken))
+    {
+      throw new RoomClassWithSameNameFoundException(RoomClassMessages.NameInHotelFound);
+    }
+
+    _mapper.Map(request, roomClassEntity);
+
+    await _roomClassRepository.UpdateAsync(roomClassEntity, cancellationToken);
+
+    await _unitOfWork.SaveChangesAsync(cancellationToken);
+  }
+}

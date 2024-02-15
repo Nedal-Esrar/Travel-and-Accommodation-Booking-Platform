cb38614c36a79f2614f7b2bd503959f5a17abed9
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:47:37 +0200

    Add get review by id query and handler

---
+++ TABP.Application/Reviews/GetById/GetReviewByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TABP.Application.Reviews.Common;
+
+namespace TABP.Application.Reviews.GetById;
+
+public record GetReviewByIdQuery(
+  Guid HotelId,
+  Guid ReviewId) : IRequest<ReviewResponse>;

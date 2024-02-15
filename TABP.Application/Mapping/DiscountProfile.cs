40df691ace6a68bede7d6300e2dc079f457a5813
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:48:54 +0200

    Add DiscountProfile mapping profile

---
+++ TABP.Application/Mapping/DiscountProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TABP.Application.Discounts.Create;
+using TABP.Domain.Entities;
+using TABP.Domain.Models;
+
+namespace TABP.Application.Mapping;
+
+public class DiscountProfile : Profile
+{
+  public DiscountProfile()
+  {
+    CreateMap<PaginatedList<Discount>, PaginatedList<Discount>>()
+      .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+    CreateMap<Discount, Discount>();
+    CreateMap<CreateDiscountCommand, Discount>();
+  }
+}

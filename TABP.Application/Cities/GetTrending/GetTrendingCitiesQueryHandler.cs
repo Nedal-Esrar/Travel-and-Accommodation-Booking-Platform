e877fb811f21b763e0e584f7a2cf5bb181f4b08b
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:04:49 +0200

    Add get trending cities query and handler

---
+++ TABP.Application/Cities/GetTrending/GetTrendingCitiesQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+
+namespace TABP.Application.Cities.GetTrending;
+
+public class GetTrendingCitiesQueryHandler : IRequestHandler<GetTrendingCitiesQuery, IEnumerable<TrendingCityResponse>>
+{
+  private readonly ICityRepository _cityRepository;
+  private readonly IMapper _mapper;
+
+  public GetTrendingCitiesQueryHandler(ICityRepository cityRepository, IMapper mapper)
+  {
+    _cityRepository = cityRepository;
+    _mapper = mapper;
+  }
+
+  public async Task<IEnumerable<TrendingCityResponse>> Handle(GetTrendingCitiesQuery request,
+    CancellationToken cancellationToken)
+  {
+    if (request.Count <= 0)
+    {
+      throw new InvalidDataException();
+    }
+
+    var cities = await _cityRepository.GetMostVisitedAsync(request.Count, cancellationToken);
+
+    return _mapper.Map<IEnumerable<TrendingCityResponse>>(cities);
+  }
+}

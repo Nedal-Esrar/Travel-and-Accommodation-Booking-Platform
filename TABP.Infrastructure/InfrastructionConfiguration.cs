d22ebd9e370fff56a89d2c8a1d4d0605b8de1ca0
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:36:45 +0200

    Add Infrastruction configuration

---
+++ TABP.Infrastructure/InfrastructionConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TABP.Domain.Interfaces.Services;
+using TABP.Infrastructure.Auth.Jwt;
+using TABP.Infrastructure.Persistence;
+using TABP.Infrastructure.Services.Date;
+using TABP.Infrastructure.Services.Email;
+using TABP.Infrastructure.Services.Pdf;
+
+namespace TABP.Infrastructure;
+
+public static class DependencyInjection
+{
+  public static IServiceCollection AddInfrastructure(
+    this IServiceCollection services, 
+    IConfiguration config)
+  {
+    services.AddPersistenceInfrastructure(config)
+      .AddAuthInfrastructure(config)
+      .AddEmailInfrastructure(config)
+      .AddPdfInfrastructure()
+      .AddScoped<IDateTimeProvider, DateTimeProvider>();
+
+    return services;
+  }
+}

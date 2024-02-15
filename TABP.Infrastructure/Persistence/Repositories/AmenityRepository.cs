68a5c06568ad6f3088a045fa4767ef0e5b225e7c
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:27:47 +0200

    Add `AmenityRepository`

---
+++ TABP.Infrastructure/Persistence/Repositories/AmenityRepository.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TABP.Domain.Entities;
+using TABP.Domain.Exceptions;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Messages;
+using TABP.Domain.Models;
+using TABP.Infrastructure.Persistence.DbContexts;
+using TABP.Infrastructure.Persistence.Extensions;
+using TABP.Infrastructure.Persistence.Helpers;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+
+public class AmenityRepository(HotelBookingDbContext context) : IAmenityRepository
+{
+  public async Task<Amenity?> GetByIdAsync(Guid id,
+    CancellationToken cancellationToken = default)
+  {
+    return await context.Amenities
+      .FindAsync([id], cancellationToken);
+  }
+
+  public async Task<bool> ExistsByIdAsync(Guid id,
+    CancellationToken cancellationToken = default)
+  {
+    return await context.Amenities.AnyAsync(
+      a => a.Id == id, cancellationToken);
+  }
+
+  public async Task<bool> ExistsByNameAsync(string name,
+    CancellationToken cancellationToken = default)
+  {
+    return await context.Amenities.AnyAsync(
+      a => a.Name == name, cancellationToken);
+  }
+
+  public async Task<Amenity> CreateAsync(Amenity amenity,
+    CancellationToken cancellationToken = default)
+  {
+    ArgumentNullException.ThrowIfNull(amenity);
+    
+    var createdAmenity = await context.Amenities
+      .AddAsync(amenity, cancellationToken);
+
+    return createdAmenity.Entity;
+  }
+
+  public async Task UpdateAsync(Amenity amenity, 
+    CancellationToken cancellationToken = default)
+  {
+    ArgumentNullException.ThrowIfNull(amenity);
+    
+    if (!await context.Amenities.AnyAsync(
+          a => a.Id == amenity.Id, cancellationToken))
+      throw new NotFoundException(AmenityMessages.WithIdNotFound);
+
+    context.Amenities.Update(amenity);
+  }
+
+  public async Task<PaginatedList<Amenity>> GetAsync(
+    PaginationQuery<Amenity> query,
+    CancellationToken cancellationToken = default)
+  {
+    var queryable = context.Amenities
+      .Where(query.Filter)
+      .Sort(SortingExpressions.GetAmenitySortExpression(query.SortColumn), query.SortOrder);
+
+    var itemsToReturn = await queryable
+      .GetPage(query.PageNumber, query.PageSize)
+      .AsNoTracking()
+      .ToListAsync(cancellationToken);
+
+    return new PaginatedList<Amenity>(
+      itemsToReturn,
+      await queryable.GetPaginationMetadataAsync(
+        query.PageNumber,
+        query.PageSize));
+  }
+}

b3f756f83e5fc1edfadd744af8dcf25237f7cd33
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:48:05 +0200

    Add update review command and handler

---
+++ TABP.Application/Reviews/Update/UpdateReviewCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TABP.Application.Reviews.Update;
+
+public record UpdateReviewCommand(
+  Guid ReviewId,
+  Guid GuestId,
+  Guid HotelId,
+  string Content,
+  int Rating) : IRequest;

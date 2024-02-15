ac2b099adc5e853f9d0afdc3e7ac8d297746865a
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 12:45:54 +0200

    add get owners query and handler

---
+++ TABP.Application/Owners/Get/GetOwnersQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TABP.Application.Owners.Common;
+using TABP.Domain.Enums;
+using TABP.Domain.Models;
+
+namespace TABP.Application.Owners.Get;
+
+public record GetOwnersQuery(
+  string? SearchTerm,
+  SortOrder? SortOrder,
+  string? SortColumn,
+  int PageNumber,
+  int PageSize) : IRequest<PaginatedList<OwnerResponse>>;

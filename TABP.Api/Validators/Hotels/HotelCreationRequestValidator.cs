c5f7895835a4002cbd9b7a2cffeffcec4eae04dd
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:56:30 +0200

    Add hotel DTOs validators

---
+++ TABP.Api/Validators/Hotels/HotelCreationRequestValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using TABP.Api.Dtos.Hotels;
+using TABP.Application.Extensions.Validation;
+
+namespace TABP.Api.Validators.Hotels;
+
+public class HotelCreationRequestValidator : AbstractValidator<HotelCreationRequest>
+{
+  public HotelCreationRequestValidator()
+  {
+    RuleFor(x => x.CityId)
+      .NotEmpty();
+
+    RuleFor(x => x.OwnerId)
+      .NotEmpty();
+
+    RuleFor(x => x.Name)
+      .NotEmpty()
+      .ValidName(3, 30);
+
+    RuleFor(x => x.StarRating)
+      .NotEmpty()
+      .InclusiveBetween(1, 5);
+
+    RuleFor(x => x.Longitude)
+      .NotEmpty()
+      .InclusiveBetween(-90, 90);
+
+    RuleFor(x => x.Latitude)
+      .NotEmpty()
+      .InclusiveBetween(-180, 180);
+
+    RuleFor(x => x.BriefDescription)
+      .MaximumLength(100);
+
+    RuleFor(x => x.Description)
+      .MaximumLength(100);
+
+    RuleFor(x => x.PhoneNumber)
+      .NotEmpty()
+      .PhoneNumber();
+  }
+}
---
+++ TABP.Api/Validators/Hotels/HotelSearchRequestValidator.cs
@@ -0,0 +1,76 @@
+﻿using FluentValidation;
+using TABP.Api.Dtos.Hotels;
+using TABP.Api.Validators.Common;
+
+namespace TABP.Api.Validators.Hotels;
+
+public class HotelSearchRequestValidator : AbstractValidator<HotelSearchRequest>
+{
+  public HotelSearchRequestValidator()
+  {
+    Include(new ResourcesQueryRequestValidator());
+
+    RuleFor(x => x.CheckInDateUtc)
+      .NotEmpty()
+      .GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow));
+
+    RuleFor(x => x.CheckOutDateUtc)
+      .NotEmpty()
+      .GreaterThanOrEqualTo(x => x.CheckInDateUtc);
+
+    RuleFor(x => x.NumberOfAdults)
+      .NotEmpty()
+      .GreaterThanOrEqualTo(1);
+
+    RuleFor(x => x.NumberOfChildren)
+      .NotEmpty()
+      .GreaterThanOrEqualTo(0);
+
+    RuleFor(x => x.NumberOfRooms)
+      .NotEmpty()
+      .GreaterThanOrEqualTo(1);
+
+    When(x => x.MinStarRating is not null, () =>
+    {
+      RuleFor(x => x.MinStarRating)
+        .InclusiveBetween(1, 5);
+    });
+
+    RuleFor(x => x.MinPrice)
+      .GreaterThan(0);
+
+    RuleFor(x => x.MaxPrice)
+      .GreaterThan(x => x.MinPrice ?? 0);
+
+    When(x => x.RoomTypes is not null, () =>
+    {
+      RuleForEach(x => x.RoomTypes)
+        .IsInEnum()
+        .WithMessage(HotelValidationMessages.RoomTypeNotValid);
+    });
+
+    RuleFor(x => x.SortColumn)
+      .Must(BeAValidSortColumn)
+      .WithMessage(HotelValidationMessages.SearchSortColumnNotValid);
+  }
+
+  private static bool BeAValidSortColumn(string? sortColumn)
+  {
+    if (string.IsNullOrEmpty(sortColumn))
+    {
+      return true;
+    }
+
+    var validColumns = new[]
+    {
+      "id",
+      "name",
+      "starRating",
+      "price",
+      "reviewsRating"
+    };
+
+    return Array.Exists(validColumns,
+      col => string.Equals(col, sortColumn, StringComparison.OrdinalIgnoreCase));
+  }
+}

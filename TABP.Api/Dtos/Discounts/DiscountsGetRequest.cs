f622de4383a8565e1cce9e1071355dc17a441b53
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:55:07 +0200

    Add discount DTOs

---
+++ TABP.Api/Dtos/Discounts/DiscountsGetRequest.cs
@@ -0,0 +1,11 @@
+﻿using TABP.Api.Dtos.Common;
+
+namespace TABP.Api.Dtos.Discounts;
+
+/// <param name="SortColumn">Should be empty, 'id', 'creaationDate', 'startDate', or 'endDate'.</param>
+public record DiscountsGetRequest(
+  string? SortColumn,
+  string? SortOrder,
+  int PageNumber = 1,
+  int PageSize = 10) : ResourcesQueryRequest(
+  null, SortColumn, SortOrder, PageNumber, PageSize);

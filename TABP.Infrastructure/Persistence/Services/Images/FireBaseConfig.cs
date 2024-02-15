1522731524cf38bd8932d4e47c02b04ff6a50b1e
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 14:56:52 +0200

    Add firebase storage image infrastructure

---
+++ TABP.Infrastructure/Persistence/Services/Images/FireBaseConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace TABP.Infrastructure.Persistence.Services.Images;
+
+public class FireBaseConfig
+{
+  public required string Credentials { get; set; }
+  public required string Bucket { get; set; }
+}

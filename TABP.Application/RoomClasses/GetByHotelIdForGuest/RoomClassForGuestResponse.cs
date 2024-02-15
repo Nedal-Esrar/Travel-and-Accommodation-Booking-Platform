71f294e2e064ec3491209941a143380015604403
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:32:33 +0200

    Add get room classes by hotel id for guest query and handler

---
+++ TABP.Application/RoomClasses/GetByHotelIdForGuest/RoomClassForGuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using TABP.Application.Amenities.Common;
+using TABP.Application.Discounts.GetById;
+using TABP.Domain.Enums;
+
+namespace TABP.Application.RoomClasses.GetByHotelIdForGuest;
+
+public record RoomClassForGuestResponse(
+  Guid Id,
+  string Name,
+  int AdultsCapacity,
+  int ChildrenCapacity,
+  decimal PricePerNight,
+  string? Description,
+  RoomType RoomType,
+  IEnumerable<AmenityResponse> Amenities,
+  DiscountResponse? ActiveDiscount,
+  IEnumerable<string> GalleryUrls);

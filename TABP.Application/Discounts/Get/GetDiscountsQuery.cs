a6788fa5c54d87e7fdaa61a39e0dd06267c30c4b
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:35:08 +0200

    Add get discounts for room class query and handler

---
+++ TABP.Application/Discounts/Get/GetDiscountsQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using TABP.Application.Discounts.GetById;
+using TABP.Domain.Enums;
+using TABP.Domain.Models;
+
+namespace TABP.Application.Discounts.Get;
+
+public record GetDiscountsQuery(
+  Guid RoomClassId,
+  SortOrder? SortOrder,
+  string? SortColumn,
+  int PageNumber,
+  int PageSize) : IRequest<PaginatedList<DiscountResponse>>;
---
+++ TABP.Application/Discounts/Get/GetDiscountsQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MediatR;
+using TABP.Application.Discounts.GetById;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+using TABP.Domain.Exceptions;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Messages;
+using TABP.Domain.Models;
+
+namespace TABP.Application.Discounts.Get;
+
+public class GetDiscountsQueryHandler : IRequestHandler<GetDiscountsQuery, PaginatedList<DiscountResponse>>
+{
+  private readonly IDiscountRepository _discountRepository;
+  private readonly IMapper _mapper;
+  private readonly IRoomClassRepository _roomClassRepository;
+
+  public GetDiscountsQueryHandler(
+    IRoomClassRepository roomClassRepository, 
+    IDiscountRepository discountRepository,
+    IMapper mapper)
+  {
+    _roomClassRepository = roomClassRepository;
+    _discountRepository = discountRepository;
+    _mapper = mapper;
+  }
+
+  public async Task<PaginatedList<DiscountResponse>> Handle(
+    GetDiscountsQuery request,
+    CancellationToken cancellationToken = default)
+  {
+    if (!await _roomClassRepository.ExistsByIdAsync(request.RoomClassId, cancellationToken))
+    {
+      throw new NotFoundException(RoomClassMessages.NotFound);
+    }
+
+    var query = new PaginationQuery<Discount>(
+      d => d.RoomClassId == request.RoomClassId,
+      request.SortOrder ?? SortOrder.Ascending,
+      request.SortColumn,
+      request.PageNumber,
+      request.PageSize);
+
+    var owners = await _discountRepository.GetAsync(query, cancellationToken);
+
+    return _mapper.Map<PaginatedList<DiscountResponse>>(owners);
+  }
+}

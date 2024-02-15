f99530be415048e68369741ea1b528ba3a002155
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 16:05:51 +0200

    Add images and common DTOs validators

---
+++ TABP.Api/Validators/Common/CommonValidationMessages.cs
@@ -0,0 +1,6 @@
+﻿namespace TABP.Api.Validators.Common;
+
+public static class CommonValidationMessages
+{
+  public const string SortOrderNotValid = "Sort order must be empty or 'asc' or 'desc'.";
+}

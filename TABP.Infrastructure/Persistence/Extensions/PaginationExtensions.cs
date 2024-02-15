944519b14830bf2284a9bcfe464f5552ffac5dde
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:24:20 +0200

    Add `PaginationExtensions`, `SortingExtensions`, and `SortingExpressions`

---
+++ TABP.Infrastructure/Persistence/Extensions/PaginationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TABP.Domain.Models;
+
+namespace TABP.Infrastructure.Persistence.Extensions;
+
+public static class PaginationExtensions
+{
+  public static IQueryable<TItem> GetPage<TItem>(
+    this IQueryable<TItem> queryable,
+    int pageNumber, int pageSize)
+  {
+    return queryable.Skip(pageSize * (pageNumber - 1))
+      .Take(pageSize);
+  }
+
+  public static async Task<PaginationMetadata> GetPaginationMetadataAsync<TItem>(
+    this IQueryable<TItem> queryable,
+    int pageNumber, int pageSize)
+  {
+    return new PaginationMetadata(
+      await queryable.CountAsync(),
+      pageNumber,
+      pageSize);
+  }
+}

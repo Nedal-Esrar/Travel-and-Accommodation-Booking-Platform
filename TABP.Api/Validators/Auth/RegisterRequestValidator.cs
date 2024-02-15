e43092b14d39150e63664ed8ffff5d77d0ec5df4
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:51:53 +0200

    Add auth DTOs validators

---
+++ TABP.Api/Validators/Auth/RegisterRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using TABP.Api.Dtos.Auth;
+using TABP.Application.Extensions.Validation;
+
+namespace TABP.Api.Validators.Auth;
+
+public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+  public RegisterRequestValidator()
+  {
+    RuleFor(x => x.FirstName)
+      .NotEmpty()
+      .ValidName(3, 30);
+
+    RuleFor(x => x.LastName)
+      .NotEmpty()
+      .ValidName(3, 30);
+
+    RuleFor(x => x.Email)
+      .NotEmpty()
+      .EmailAddress();
+
+    RuleFor(x => x.Password)
+      .NotEmpty()
+      .StrongPassword();
+  }
+}

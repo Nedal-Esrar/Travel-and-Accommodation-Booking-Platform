bc5c48d4824f954f459d8566665c7b8a8d2fe211
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 13:14:44 +0200

    Add get hotel by id for guest query and handler

---
+++ TABP.Application/Hotels/GetForGuestById/HotelForGuestResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace TABP.Application.Hotels.GetForGuestById;
+
+public record HotelForGuestResponse(
+  Guid Id,
+  string Name,
+  double Longitude,
+  double Latitude,
+  string CityName,
+  string CountryName,
+  int StarRating,
+  float ReviewsRating,
+  string? BriefDescription,
+  string? Description,
+  string? ThumbnailUrl,
+  IEnumerable<string> GalleryUrls);

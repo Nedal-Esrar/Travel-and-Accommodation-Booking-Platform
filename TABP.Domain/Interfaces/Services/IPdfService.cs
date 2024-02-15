66479f24ed6736f146c3fd969e491aa28e77407a
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 12:34:53 +0200

    Add `IPdfService`

---
+++ TABP.Domain/Interfaces/Services/IPdfService.cs
@@ -0,0 +1,6 @@
+﻿namespace TABP.Domain.Interfaces.Services;
+
+public interface IPdfService
+{
+  Task<byte[]> GeneratePdfFromHtmlAsync(string html, CancellationToken cancellationToken = default);
+}

adcae6147b5cb1eca2f6d4a7fdcf067cf4d56b2b
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:55:31 +0200

    Add discount DTOs validator

---
+++ TABP.Api/Validators/Discounts/DiscountsValidationMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace TABP.Api.Validators.Discounts;
+
+public static class DiscountsValidationMessages
+{
+  public const string ShouldBeAValidSortColumn =
+    "Sort Column must be empty or 'id', 'creationDate', 'StartDate', or 'EndDate' of the Discount.";
+}

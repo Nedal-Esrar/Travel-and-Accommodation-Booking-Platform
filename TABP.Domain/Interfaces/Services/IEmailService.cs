9a722f956e866dcd3c5c35f21dbd0318b9410cbc
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 12:34:26 +0200

    Add `IEmailService`

---
+++ TABP.Domain/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using TABP.Domain.Models;
+
+namespace TABP.Domain.Interfaces.Services;
+
+public interface IEmailService
+{
+  Task SendAsync(EmailRequest emailRequest, CancellationToken cancellationToken = default);
+}

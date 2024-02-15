119ba43d4c25546f3537ffe68c067baad0e38f9d
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:54:27 +0200

    Add city DTOs validators

---
+++ TABP.Api/Validators/Cities/CitiesValidationMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace TABP.Api.Validators.Cities;
+
+public static class CitiesValidationMessages
+{
+  public const string ShouldBeAValidSortColumn =
+    "Sort Column must be empty or 'id', 'name', 'country', or 'postOffice' of the city.";
+}
---
+++ TABP.Api/Validators/Cities/CityCreationRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using TABP.Api.Dtos.Cities;
+using TABP.Application.Extensions.Validation;
+
+namespace TABP.Api.Validators.Cities;
+
+public class CityCreationRequestValidator : AbstractValidator<CityCreationRequest>
+{
+  public CityCreationRequestValidator()
+  {
+    RuleFor(c => c.Name)
+      .NotEmpty()
+      .ValidName(3, 30);
+
+    RuleFor(c => c.Country)
+      .NotEmpty()
+      .ValidName(3, 30);
+
+    RuleFor(c => c.PostOffice)
+      .NotEmpty()
+      .ValidNumericString(5);
+  }
+}

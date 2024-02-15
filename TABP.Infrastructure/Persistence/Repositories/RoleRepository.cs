fd551835d8d4fb5410ea013122befa0ca0686ff8
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 15:30:38 +0200

    Add `RoleRepository`

---
+++ TABP.Infrastructure/Persistence/Repositories/RoleRepository.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TABP.Domain.Entities;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Infrastructure.Persistence.DbContexts;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+
+public class RoleRepository(HotelBookingDbContext context) : IRoleRepository
+{
+  public async Task<Role?> GetByNameAsync(string name, CancellationToken cancellationToken = default)
+  {
+    return await context.Roles
+      .SingleOrDefaultAsync(r => r.Name == name, cancellationToken);
+  }
+}

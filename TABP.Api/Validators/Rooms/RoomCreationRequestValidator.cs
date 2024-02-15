b6dc93019fac7a637d7af8fd687def2b4eec3a0c
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-15 16:04:39 +0200

    Add rooms DTOs validators

---
+++ TABP.Api/Validators/Rooms/RoomCreationRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using TABP.Api.Dtos.Rooms;
+
+namespace TABP.Api.Validators.Rooms;
+
+public class RoomCreationRequestValidator : AbstractValidator<RoomCreationRequest>
+{
+  public RoomCreationRequestValidator()
+  {
+    RuleFor(x => x.Number)
+      .NotEmpty();
+  }
+}
---
+++ TABP.Api/Validators/Rooms/RoomUpdateRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using TABP.Api.Dtos.Rooms;
+
+namespace TABP.Api.Validators.Rooms;
+
+public class RoomUpdateRequestValidator : AbstractValidator<RoomUpdateRequest>
+{
+  public RoomUpdateRequestValidator()
+  {
+    RuleFor(x => x.Number)
+      .NotEmpty();
+  }
+}

f95349336a526159d407664039e8e75e7b8e55fd
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-23 15:07:09 +0200

    Add `JwtAuthConfigValidator` and validate `JwtAuthConfig` using `FluentValidation`

---
+++ TABP.Infrastructure/Auth/Jwt/JwtAuthConfigValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace TABP.Infrastructure.Auth.Jwt;
+
+public class JwtAuthConfigValidator : AbstractValidator<JwtAuthConfig>
+{
+  public JwtAuthConfigValidator()
+  {
+    RuleFor(x => x.Key)
+      .NotEmpty();
+
+    RuleFor(x => x.Issuer)
+      .NotEmpty();
+
+    RuleFor(x => x.Audience)
+      .NotEmpty();
+
+    RuleFor(x => x.LifetimeMinutes)
+      .NotEmpty()
+      .GreaterThan(0);
+  }
+}

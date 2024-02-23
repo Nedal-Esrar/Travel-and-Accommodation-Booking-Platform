fbdb39c41c35d6bbfffaa8d50ff5a9423e25d4f1
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-23 14:59:39 +0200

    Add `FluentValidateOptions` and `ValidationMessages` classes
    `FluentValidateOptions` is and implementation of `IValidateOptions` interface and it used to validate options using `FluentValidation`.

---
+++ TABP.Infrastructure/Common/OptionsValidation/FluentValidateOptions.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace TABP.Infrastructure.Common.OptionsValidation;
+
+public class FluentValidateOptions<TOptions> : IValidateOptions<TOptions>
+  where TOptions : class
+{
+  private readonly IServiceProvider _serviceProvider;
+
+  public FluentValidateOptions(IServiceProvider serviceProvider)
+  {
+    _serviceProvider = serviceProvider;
+  }
+
+  public ValidateOptionsResult Validate(string? name, TOptions options)
+  {
+    ArgumentNullException.ThrowIfNull(options);
+
+    using var scope = _serviceProvider.CreateScope();
+
+    var validator = scope.ServiceProvider.GetRequiredService<IValidator<TOptions>>();
+
+    var results = validator.Validate(options);
+
+    if (results.IsValid)
+    {
+      return ValidateOptionsResult.Success;
+    }
+
+    var optionsTypeName = typeof(TOptions).Name;
+
+    var errors = results.Errors.Select(e
+      => ValidationMessages.GenerateValidationFailureMessages(optionsTypeName, e.PropertyName, e.ErrorMessage));
+
+    return ValidateOptionsResult.Fail(errors);
+  }
+}
---
+++ TABP.Infrastructure/Common/OptionsValidation/ValidationMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace TABP.Infrastructure.Common.OptionsValidation;
+
+public static class ValidationMessages
+{
+  public static string GenerateValidationFailureMessages(string optionsType, string propertyName, string errorMessages)
+  {
+    return $"Fluent validation failed for '{optionsType}.{propertyName}' with the error: '{errorMessages}'.";
+  }
+}

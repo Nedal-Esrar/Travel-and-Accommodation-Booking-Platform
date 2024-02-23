ba57ab67bfe1c92a750a0d69b878bcbdcfa1fdae
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-02-23 15:09:27 +0200

    Add `ValidJson` extension method

---
+++ TABP.Infrastructure/Common/OptionsValidation/ValidationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using FluentValidation;
+
+namespace TABP.Infrastructure.Common.OptionsValidation;
+
+public static class ValidationExtensions
+{
+  public static IRuleBuilderOptions<T, string> ValidJson<T>(this IRuleBuilder<T, string> ruleBuilder)
+  {
+    return ruleBuilder
+      .Must(IsValidJson)
+      .WithMessage(ValidationMessages.InvalidJson);
+  }
+
+  private static bool IsValidJson(string json)
+  {
+    try
+    {
+      _ = JsonDocument.Parse(json);
+      
+      return true;
+    }
+    catch (JsonException)
+    {
+      return false;
+    }
+  }
+}
--- TABP.Infrastructure/Common/OptionsValidation/ValidationMessages.cs
+++ TABP.Infrastructure/Common/OptionsValidation/ValidationMessages.cs
@@ -2,6 +2,8 @@
 
 public static class ValidationMessages
 {
+  public const string InvalidJson = "'{PropertyName}' is not a valid JSON string.";
+
   public static string GenerateValidationFailureMessages(string optionsType, string propertyName, string errorMessages)
   {
     return $"Fluent validation failed for '{optionsType}.{propertyName}' with the error: '{errorMessages}'.";

922fff4592de1512fc8e0da502d6a37f2bb45506
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-03-12 10:55:19 +0200

    Add `ProjectDependenciesTests`
    To enforce architecture project dependencies rules

---
+++ TABP.Architecture.Tests/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace TABP.Architecture.Tests;
+
+public static class Constants
+{
+  public const string DomainAssembly = "TABP.Domain";
+  public const string ApplicationAssembly = "TABP.Application";
+  public const string InfrastructureAssembly = "TABP.Infrastructure";
+  
+  public const string ApplicationNamespace = "TABP.Application";
+  public const string InfrastructureNamespace = "TABP.Infrastructure";
+  public const string ApiNamespace = "TABP.Api";
+}
---
+++ TABP.Architecture.Tests/ProjectsDependenciesTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using NetArchTest.Rules;
+using static TABP.Architecture.Tests.Constants;
+
+namespace TABP.Architecture.Tests;
+
+public class ProjectsDependenciesTests
+{
+  [Fact]
+  public void Domain_ShouldNotHaveDependencyOn_AnyLayer()
+  {
+    var assembly = Assembly.Load(DomainAssembly);
+
+    var otherProjects = new[]{
+      ApplicationNamespace,
+      InfrastructureNamespace,
+      ApiNamespace
+    };
+
+    var result = Types.InAssembly(assembly)
+      .ShouldNot()
+      .HaveDependencyOnAll(otherProjects)
+      .GetResult();
+
+    result.IsSuccessful.Should().BeTrue();
+  }
+  
+  [Fact]
+  public void Application_ShouldNotHaveDependencyOn_ExternalLayers()
+  {
+    var assembly = Assembly.Load(ApplicationAssembly);
+
+    var otherProjects = new[]{
+      InfrastructureNamespace,
+      ApiNamespace
+    };
+
+    var result = Types.InAssembly(assembly)
+      .ShouldNot()
+      .HaveDependencyOnAll(otherProjects)
+      .GetResult();
+
+    result.IsSuccessful.Should().BeTrue();
+  }
+  
+  [Fact]
+  public void Infrastructure_ShouldNotHaveDependencyOn_OtherExternalLayers()
+  {
+    var assembly = Assembly.Load(InfrastructureAssembly);
+
+    var otherProjects = new[]{
+      ApiNamespace
+    };
+
+    var result = Types.InAssembly(assembly)
+      .ShouldNot()
+      .HaveDependencyOnAll(otherProjects)
+      .GetResult();
+
+    result.IsSuccessful.Should().BeTrue();
+  }
+}

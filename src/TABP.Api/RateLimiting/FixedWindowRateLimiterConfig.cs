f043e2d0f639a6324b5c68cb722efaff5ce8d232
Nedal-Esrar <nedalesrarahmad@gmail.com>
2024-04-21 19:12:06 +0300

    Migrate rate limiting configuration to utilize appsettings.json with options validation
    This is done to replace hard-coded configuration for rate limiting.

---
+++ src/TABP.Api/RateLimiting/FixedWindowRateLimiterConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.RateLimiting;
+
+namespace TABP.Api.RateLimiting;
+
+public class FixedWindowRateLimiterConfig
+{
+  public required int PermitLimit { get; set; }
+  public required double TimeWindowSeconds { get; set; }
+  public required QueueProcessingOrder QueueProcessingOrder { get; set; }
+  public required int QueueLimit { get; set; }
+}
---
+++ src/TABP.Api/RateLimiting/FixedWindowRateLimiterConfigValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace TABP.Api.RateLimiting;
+
+public class FixedWindowRateLimiterConfigValidator : AbstractValidator<FixedWindowRateLimiterConfig>
+{
+  public FixedWindowRateLimiterConfigValidator()
+  {
+    RuleFor(x => x.PermitLimit)
+      .NotEmpty()
+      .GreaterThan(0);
+
+    RuleFor(x => x.TimeWindowSeconds)
+      .NotEmpty()
+      .GreaterThan(0);
+
+    RuleFor(x => x.QueueProcessingOrder)
+      .NotNull()
+      .IsInEnum();
+
+    RuleFor(x => x.QueueLimit)
+      .NotEmpty()
+      .GreaterThan(0);
+  }
+}
